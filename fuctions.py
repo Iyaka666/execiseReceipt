dbd8043390c895f8b630a4690e57ec3fe8b258c7
Iyaka666 <duendecum@gmail.com>
2022-06-02 14:45:18 -0500

    move method interface to functions

--- fuctions.py
+++ fuctions.py
@@ -12,3 +12,94 @@ def show2Column(array1 = (), array2 = ()):
                 table += array1[i] + ": " + array2[i]
             i += 1
     return table
+
+def validateString(messageInput = ' '):
+    s = ' '
+    while not s.isalpha():
+            s = ((input(messageInput + " --> ")).strip()).lower()
+            if s.isalpha() == False:
+                print("Solo ingrese letras, por favor")
+    return s
+
+def validateAddress(messageInput = "Ingrese la dirección", ):
+    s = ' '
+    charactersToValidate = "#.-"
+    s = input((messageInput.strip()).lower() + " --> ")
+    listBool = []
+    flagWhileMain = True
+    flagWhile2 = False
+    verifyLoop1 = 0
+    while flagWhileMain:
+        for i in s:
+            if charactersToValidate.find(i) == -1: # search in the String if the characters to validate with its
+                listBool.append(False)
+            else:
+                listBool.append(True)
+        while verifyLoop1 < len(listBool):
+            if listBool[verifyLoop1]:
+                flagWhileMain = False
+                break
+            verifyLoop1 += 1
+        
+        if flagWhileMain == False:
+            pass
+        elif verifyLoop1 == len(listBool) + 1:
+                flagWhile2 = True
+        while flagWhile2:
+            print("has introducido un carácter que no pertenece a estos \nPor favor ingrese alguno de estos --> '#' , '.' , '-'")
+            s = input(messageInput.strip() +" nuevamente"+ " --> ")
+            break
+    
+    return s
+
+def validateDigitAndNumberZRange(quantityFinalDigit , quantityInitialDigit = 0, messageInput = "Ingrese el número"):
+    nuip = ' ' #this value is the more important data for function is long number to validate 
+    value = ' '
+    keepWhile = True
+    while keepWhile:
+        nuip = input( (messageInput.strip()).lower() +" --> ")
+        for i in messageInput.split(" "):
+            if i == len(messageInput.split(" ")) - 1:
+                value = i.lower()
+        while not (len(nuip) >= quantityInitialDigit and len(nuip) <= quantityFinalDigit) and (nuip.strip()).isdigit(): 
+            error1 = f"La cantidad de digitos del {value} solo entre {quantityInitialDigit} a {quantityFinalDigit}, por favor"
+            error2 = "Ingrese solo números entre 0 a 9, por favor"
+            if (len(nuip) >= quantityInitialDigit and len(nuip) <= quantityFinalDigit) == False:
+                print(error1)
+                break
+            elif (nuip.strip()).isdigit() == False:
+                print(error2)
+                break
+            elif ((len(nuip) >= quantityInitialDigit and len(nuip) <= quantityFinalDigit) and (nuip.strip()).isdigit()) == False:
+                print(error1 + " además " + error2.lower())
+                break
+            else:
+                keepWhile = False
+                break
+    return int(nuip)
+
+
+def validateDigitAndNumberZ(quantityDigits, messageInput = "Ingrese el número"):
+    nuip = value = ' '
+    keepWhile = True
+    while keepWhile:
+        nuip = input( (messageInput.strip()).lower() +" --> ")
+        for i in messageInput.split(" "):
+            if i == len(messageInput.split(" ")) - 1:
+                value = i.lower()
+        while not (len(nuip) == quantityDigits) and (nuip.strip()).isdigit(): 
+            error1 = f"La cantidad de digitos del {value} debe ser {quantityDigits}, por favor"
+            error2 = "Ingrese solo números entre 0 a 9, por favor"
+            if (len(nuip) >= quantityDigits) == False:
+                print(error1)
+                break
+            elif (nuip.strip()).isdigit() == False:
+                print(error2)
+                break
+            elif ((len(nuip) >= quantityDigits) and (nuip.strip()).isdigit()) == False:
+                print(error1 + " además " + error2.lower())
+                break
+            else:
+                keepWhile = False
+                break
+    return int(nuip)
